3b7b13077deb562007612cb9cd5f4f2479da3e92
heiwa4126 <heiwa4126@gmail.com>
2026-07-29 18:35:41 +0900

    update

--- src/submit.ts
+++ src/submit.ts
@@ -1,13 +1,27 @@
 import { parseOrderObject } from "../lib/order_schema.ts";
 
+function disableSubmitButton(form: HTMLFormElement, status: boolean = true) {
+	const submitButton = form.querySelector('button[type="submit"]') as HTMLButtonElement;
+
+	if (submitButton) {
+		submitButton.disabled = status;
+	}
+}
+
 async function submitHook(e: SubmitEvent) {
 	e.preventDefault();
 
-	const formData = new FormData(e.currentTarget as HTMLFormElement);
+	const form = e.currentTarget as HTMLFormElement;
+	disableSubmitButton(form, true);
+	const formData = new FormData(form);
 	const payload = Object.fromEntries(formData.entries());
 	payload.name = " ①"; // DEBUG
 	const result = parseOrderObject(payload);
+
 	console.log(result);
+	setTimeout(() => {
+		disableSubmitButton(form, false);
+	}, 500);
 }
 
 export function setSubmitHook(id: string) {
